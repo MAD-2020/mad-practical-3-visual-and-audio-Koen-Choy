a0f0923290e0e0f529af05094bcb72b741dfd501
Koen-Choy <64003030+Koen-Choy@users.noreply.github.com>
2020-05-14 18:15:24 +0800

    Update MainActivity.java

--- app/src/main/java/sg/edu/np/mad/week3/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/week3/MainActivity.java
@@ -1,8 +1,11 @@
-package sg.edu.np.mad.week3;
+package sg.edu.np.connect.week3practical;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        VideoView view = findViewById(R.id.videoView);
+        view.setVideoURI(
+                Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.rabbid) );
+        view.start();
+        MediaPlayer media = MediaPlayer.create(this, R.raw.baby_laughing);
+        media.start();
+//once done with the playback
+        media.release();
+        media = null;
+
+
+
     }
+
 }
